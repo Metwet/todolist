92b59735bf97419b39477cae52d8926e2308df46
Metwet <tselm.matv@gmail.com>
2023-06-21 00:19:53 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { AboutComponent } from './modules/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: `login`, component: LoginComponent},
+  { path: `todolist`, component: TodoListComponent},
+  { path: `about`, component: AboutComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
