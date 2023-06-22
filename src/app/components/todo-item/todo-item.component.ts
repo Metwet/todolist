22525e79948c9e1d99f6561435efcd5ed914bdc4
Metwet <tselm.matv@gmail.com>
2023-06-22 03:43:56 +0300

    add delete item

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TodoItem, TodoStatus } from 'src/app/models/todo-item.model';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,6 +8,11 @@ import { TodoItem, TodoStatus } from 'src/app/models/todo-item.model';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent {
+  constructor (private todoService: TodoService) {}
   @Input() todoItem!: TodoItem;
   todoStatus = TodoStatus;
+
+  deleteTodoItem(): void {
+    this.todoService.deleteTodoItems(this.todoItem);
+  }
 }
